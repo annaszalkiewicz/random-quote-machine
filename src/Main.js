6e905f6d50d05aaa8c3fd1b3ee441e48bd8aa1af
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-10-06 21:24:50 +0200

    Fetch random quote from API

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 
 class Main extends Component {
 
+	constructor(props) {
+		super(props);
+
+		this.showRandom = this.showRandom.bind(this);
+		this.showError = this.showError.bind(this);
+	}
+
+	componentDidMount = () => {
+
+		fetch('https://talaikis.com/api/quotes/random/')
+		.then(response => response.json())
+		.then(this.showRandom())
+		.catch(e => this.showError());
+
+	}
+
+	showRandom = () => {
+		console.log('Success!');
+	}
+
+	showError = () => {
+		console.log('Error');
+	}
+
 	render() {
 		return (
 			<main className="main">
