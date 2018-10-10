531f96ff6f34ed65d7d9191223087444fbda78de
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-10-10 22:31:18 +0200

    Add share Tumblr button

--- src/Share.js
+++ src/Share.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+import { faTwitterSquare, faTumblrSquare } from '@fortawesome/free-brands-svg-icons'
 
 
 class Share extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+	
 
 	render() {
 
 		const { quote, author } = this.props;
 
-		return(
+		return (
 
-			<div className="tweet">
-				
-					<a href={`https://twitter.com/intent/tweet?text=${quote}%20by%20${author}&hashtags=quote`}>
-					<FontAwesomeIcon icon={faTwitterSquare} className="twitter" onClick={this.tweet} />
+			<div className="share"> a
+
+				<button className="facebook">
+					<a href="https://www.tumblr.com/share"  data-posttype="quote" data-content={quote} data-caption={author} target="_blank" rel="noopener noreferrer">
+						<FontAwesomeIcon icon={faTumblrSquare} />
 					</a>
+				</button>
+				<button className="tweet">
+					<a href={`https://twitter.com/intent/tweet?text=${quote}%20by%20${author}&hashtags=quote`}>
+						<FontAwesomeIcon icon={faTwitterSquare} className="twitter" onClick={this.tweet} />
+					</a>
+				</button>
 
 			</div>
 
