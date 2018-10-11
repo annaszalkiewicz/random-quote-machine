bddd8b83ea2abcd00f6361f7dfb1908ed9d306ff
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-10-11 23:22:15 +0200

    Remove useless constructor in Share component

--- src/Share.js
+++ src/Share.js
@@ -3,13 +3,7 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitterSquare, faTumblrSquare } from '@fortawesome/free-brands-svg-icons'
 
-
 class Share extends Component {
-
-	constructor(props) {
-		super(props);
-	}
-	
 
 	render() {
 
