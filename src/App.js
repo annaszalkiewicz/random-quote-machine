306a1bc58cb4079e8943271e9af7f3a4f321fa16
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-10-13 22:42:08 +0200

    Add random background color to App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,45 @@ import React, { Component } from 'react';
 import Main from './Main';
 
 class App extends Component {
+
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			colors: ['#711717', '#155f60', '#6c943e', '#1e7f3f'],
+			randomColor: ''
+		}
+		this.updateColor = this.updateColor.bind(this);
+	}
+
+	componentDidMount = () => {
+
+		this.updateColor();
+
+	}
+
+	updateColor = () => {
+
+		const { colors } = this.state;
+		const random = Math.floor(Math.random() * colors.length);
+		const color = colors[random];
+
+		this.setState({ randomColor: color });
+
+	}
+	
 	render() {
+
+		const { randomColor } = this.state;
+
 		return (
-			<div className="App">
+			<div className="App" style={{background: randomColor}}>
 				<header className="header">
 					<h1 className="heading-one">Your quote for today</h1>
 				</header>
-				<Main />
+				<Main 
+					updateColor={this.updateColor}
+				/>
 				<footer className="footer">
 					<p className="copyright">Written and coded by <a href="https://mywebgraphicdesign.com" target="_blank" rel="noopener noreferrer">Anna Szalkiewicz</a>.</p>
 				</footer>
