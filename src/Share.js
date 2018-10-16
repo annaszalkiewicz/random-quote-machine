269c36b488337721b8cb15881e1f519cf13a4e16
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-10-17 01:46:52 +0200

    Add animation to twiiter, tumblr and reload buttons on hover

--- src/Share.js
+++ src/Share.js
@@ -13,8 +13,8 @@ class Share extends Component {
 
 			<div className="share">
 
-				<a href="https://www.tumblr.com/share" data-posttype="quote" data-content={quote} data-caption={author} target="_blank" rel="noopener noreferrer" className="tumblr" aria-label="Share quote on Tumblr">
-					<FontAwesomeIcon icon={faTumblrSquare} size="3x" style={{ color: randomColor }} />
+				<a href="https://www.tumblr.com/share" data-posttype="quote" data-content={quote} data-caption={author} target="_blank" rel="noopener noreferrer"  aria-label="Share quote on Tumblr">
+					<FontAwesomeIcon icon={faTumblrSquare} size="3x" style={{ color: randomColor }} className="tumblr" />
 				</a>
 
 				<a href={`https://twitter.com/intent/tweet?text=${quote}%20by%20${author}&hashtags=quote`} className="tweet" id="tweet-quote" aria-label="Share quote on Twitter">
