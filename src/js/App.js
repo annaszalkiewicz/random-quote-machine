792b9b0be2370f59757889aa3a71f6ad6de84288
annaszalkiewicz <anna@mywebgraphicdesign.com>
2018-11-04 22:22:04 +0100

    Hide referral in footer if none photo is displayed

--- src/js/App.js
+++ src/js/App.js
@@ -6,7 +6,7 @@ class App extends Component {
 
 	constructor(props) {
 		super(props);
-		
+
 		this.state = {
 			colors: ['#711717', '#155f60', '#6c943e', '#1e7f3f'],
 			randomColor: '',
@@ -41,43 +41,56 @@ class App extends Component {
 
 		let tag = 'nature';
 
-			fetch(`https://api.unsplash.com/photos/random?query=${tag}&orientation=landscape`, {
-        headers: {
-            Authorization: 'Client-ID 0cf967e9a4b6704211470385b4e1678bef77ab4f36e25d48dc43708eb6715be8'
-        }
-    }).then(response => response.json())
-        .then( (data) => {
-					this.setState({ randomImage: data.urls.raw })
-					this.setState({ author: data.user.name })
-					this.setState({ link: data.user.links.html })
-				})
-        .catch( () => {
-					this.setState({ error: true })
-				});
+		fetch(`https://api.unsplash.com/photos/random?query=${tag}&orientation=landscape`, {
+			headers: {
+				Authorization: 'Client-ID 0cf967e9a4b6704211470385b4e1678bef77ab4f36e25d48dc43708eb6715be8'
+			}
+		}).then(response => response.json())
+			.then((data) => {
+				this.setState({ randomImage: data.urls.raw })
+				this.setState({ author: data.user.name })
+				this.setState({ link: data.user.links.html })
+			})
+			.catch(() => {
+				this.setState({ error: true })
+			});
 
 	}
-	
+
 	render() {
 
 		const { randomImage, randomColor, author, link, error } = this.state;
 
 		return (
-			<div className="App" style={{backgroundImage: `url(${randomImage})`, backgroundSize: "cover", backgroundAttachment: "fixed", backgroundPosition: "center", backgroundColor: randomColor, }}>
+			<div className="App" style={{ backgroundImage: `url(${randomImage})`, backgroundSize: "cover", backgroundAttachment: "fixed", backgroundPosition: "center", backgroundColor: randomColor, }}>
 				<header className="header">
 					<h1 className="heading-one">Your quote for today</h1>
 				</header>
-				<Main 
+				<Main
 					updateColor={this.updateColor}
 					randomColor={randomColor}
 					fetchImages={this.fetchImages}
 					error={error}
 				/>
 				<footer className="footer">
-					<p className="copyright">Written and coded by <a href="https://mywebgraphicdesign.com" target="_blank" rel="noopener noreferrer">Anna Szalkiewicz</a>.</p>
-					<Referral 
-					author={author}
-					link={link}
-					/>
+
+					{error &&
+						<p className="copyright">Written and coded by <a href="https://mywebgraphicdesign.com" target="_blank" rel="noopener noreferrer">Anna Szalkiewicz</a>.</p>
+					}
+
+					{navigator.onLine &&
+						<div>
+							<p className="copyright">Written and coded by <a href="https://mywebgraphicdesign.com" target="_blank" rel="noopener noreferrer">Anna Szalkiewicz</a>.</p>
+							<Referral
+								author={author}
+								link={link}
+							/>
+						</div>
+
+					}
+
+
+
 				</footer>
 			</div>
 		);
